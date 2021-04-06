5441cd9d06762b8049908e6ccb4d4adff7796a0d
NSWRO\1163524 <ashahid@woolworths.com.au>
2021-04-06 16:54:07 +1000

    Backend - Settings api

---
+++ backend/MessageReplay.Api/MessageReplay.Api/Controllers/SettingsController.cs
@@ -0,0 +1,44 @@
+﻿using MessageReplay.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MessageReplay.Api.Controllers
+{
+
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+
+    public class SettingsController : ControllerBase
+    {
+        private readonly IGlobalSettings _globalSettings;
+        private readonly ILogger<SettingsController> _logger;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="globalSettings"></param>
+        /// <param name="logger"></param>
+        public SettingsController(IGlobalSettings globalSettings, ILogger<SettingsController> logger)
+        {
+            _globalSettings = globalSettings;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult SaveGlobalSettings(string sbConnectionString)
+        {
+            _globalSettings.ServiceBusConnectionString = sbConnectionString;
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult FetchGlobalSettings()
+        {
+            return Ok(new
+            {
+                ServiceBusConnectionString = _globalSettings.ServiceBusConnectionString
+            });
+        }
+    }
+}
---
+++ backend/MessageReplay.Api/MessageReplay.Api/Services/GlobalSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageReplay.Api.Services
+{
+   
+    public interface IGlobalSettings
+    {
+        string ServiceBusConnectionString { get; set; }
+    }
+
+    public class GlobalSettings : IGlobalSettings
+    {
+        private string _serviceBusConnectionString { get; set; }
+        string IGlobalSettings.ServiceBusConnectionString
+        {
+            get => _serviceBusConnectionString;
+            set => _serviceBusConnectionString = value;
+        }
+    }
+}
